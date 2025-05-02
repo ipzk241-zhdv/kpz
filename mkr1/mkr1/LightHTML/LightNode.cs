b38a1f78627492ab7c75ead68ee83568b734cac2
Жеребцов Дмитро Вікторович <ipzk241_zhdv@student.ztu.edu.ua>
2025-05-02 22:19:39 +0300

    mkr1 init lighthtml

---
+++ mkr1/mkr1/LightHTML/LightNode.cs
@@ -0,0 +1,25 @@
+﻿namespace mkr1.LightHTML
+{
+    public abstract class LightNode
+    {
+        public abstract string OuterHTML { get; }
+        public abstract string InnerHTML { get; }
+    }
+
+    public class LightTextNode : LightNode
+    {
+        public string Text { get; }
+
+        public LightTextNode(string text)
+        {
+            Text = text;
+        }
+
+        public override string OuterHTML => Text;
+
+        public override string InnerHTML => Text;
+    }
+
+    public enum DisplayType { Block, Inline }
+    public enum ClosingType { SelfClosing, WithClosingTag }
+}
---
+++ mkr1/mkr1/Program.cs
@@ -0,0 +1,28 @@
+﻿using mkr1.LightHTML;
+
+LightHTML();
+
+static void LightHTML()
+{
+    var div = new LightElementNode("div");
+    div.AddClass("container");
+
+    var h1 = new LightElementNode("h1");
+    h1.AddChild(new LightTextNode("Welcome to LightHTML"));
+
+    var p = new LightElementNode("p");
+    p.AddChild(new LightTextNode("This is a custom markup example."));
+
+    var img = new LightElementNode("img", closing: ClosingType.SelfClosing);
+    img.AddClass("responsive");
+
+    div.AddChild(h1);
+    div.AddChild(p);
+    div.AddChild(img);
+
+    Console.WriteLine("== OuterHTML ==");
+    Console.WriteLine(div.OuterHTML);
+    Console.WriteLine();
+    Console.WriteLine("== InnerHTML ==");
+    Console.WriteLine(div.InnerHTML);
+}
