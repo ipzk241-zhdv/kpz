f8b798f0dcfc60fb3e119409f34d0a2739dda5ab
Жеребцов Дмитро Вікторович <ipzk241_zhdv@student.ztu.edu.ua>
2025-05-03 01:50:48 +0300

    mkr1 visitor

---
+++ mkr1/mkr1/Visitor/HTMLParserVisitor.cs
@@ -0,0 +1,24 @@
+﻿using mkr1.LightHTML;
+
+namespace mkr1.Visitor
+{
+    public class HTMLParserVisitor : IVisitor
+    {
+        private readonly HashSet<string> _classes = new HashSet<string>();
+        public IReadOnlyCollection<string> Classes => _classes;
+
+        private readonly HashSet<string> _texts = new HashSet<string>();
+        public IReadOnlyCollection<string> Texts => _texts;
+
+        public void Visit(LightTextNode textNode)
+        {
+            _texts.Add(textNode.InnerHTML);
+        }
+
+        public void Visit(LightElementNode elementNode)
+        {
+            foreach (var cls in elementNode.CssClasses)
+                _classes.Add(cls);
+        }
+    }
+}
---
+++ mkr1/mkr1/Visitor/IVisitor.cs
@@ -0,0 +1,10 @@
+﻿using mkr1.LightHTML;
+
+namespace mkr1.Visitor
+{
+    public interface IVisitor
+    {
+        void Visit(LightTextNode textNode);
+        void Visit(LightElementNode elementNode);
+    }
+}
